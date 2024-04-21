b61a768b952133ec16dfdc9e15037203b6fcc6d8
MohMohAung <mohmohaung737@gmail.com>
2024-04-22 00:49:37 +0630

    fix-page

--- src/page/contact/Contact.tsx
+++ src/page/contact/Contact.tsx
@@ -7,15 +7,15 @@ const Contact = () => {
         <h1 className={classes.heading}>Get In Touch</h1>
         <p className={classes.subHeading}>To Discuss your favorite books </p>
         <div className={classes.boxContainer}>
-          <h2 className={classes.boxHeading}>Let's Talk</h2>
+          <h5 className={classes.boxHeading}>Let's Talk</h5>
           <div className={classes.list}>
             <div className={classes.listContainer}>
               <div>
                 <p className={classes.text}>Phone -</p>
-                <p>09259575377</p>
+                <p className={classes.contactP}>09259575377</p>
               </div>
 
-              <div>
+              <div className={classes.contactP}>
                 Give me a call and chat directily with your friendly. We are
                 always happy to discuss favorite book.
               </div>
@@ -24,11 +24,11 @@ const Contact = () => {
             <div className={classes.listContainer}>
               <div>
                 <p className={classes.text}>Email-</p>
-                <p>mohmohaung737@gmail.com</p>
+                <p className={classes.contactP}>mohmohaung737@gmail.com</p>
               </div>
 
               <div>
-                <p>
+                <p className={classes.contactP}>
                   Send us a detailed message.We well get back to you as soon as
                   possible to disscus your word.
                 </p>
@@ -41,11 +41,11 @@ const Contact = () => {
               </div>
 
               <div>
-                <p>Follow us on Social Media</p>
+                <p className={classes.contactP}>Follow us on Social Media</p>
               </div>
             </div>
           </div>
-          <p className={classes.footerText}>Wait me your response</p>
+          <h6 className={classes.footerText}>Wait me your response</h6>
         </div>
       </div>
     </div>
--- src/page/page-item/Page.tsx
+++ src/page/page-item/Page.tsx
@@ -1,5 +1,99 @@
+import { useState } from "react";
+import classes from "./Page.module.css";
+import { FaRegFaceLaughBeam } from "react-icons/fa6";
+import { FaRegFaceKissWinkHeart } from "react-icons/fa6";
+import { FaRegFaceGrinTongueSquint } from "react-icons/fa6";
+import { FaRegFaceSadCry } from "react-icons/fa6";
+
 const Page = () => {
-  return <div>Page</div>;
+  const [showIcon, setShowIcon] = useState(false);
+  const [selectedIcon, setSelectedIcon] = useState(0);
+  const [iconList, setIconList] = useState(0);
+  const booklist = [
+    {
+      id: 1,
+      name: "Defininte Guide with Javascript",
+    },
+    {
+      id: 2,
+      name: "Defininte Guide with Javascript",
+    },
+    {
+      id: 3,
+      name: "Defininte Guide with Javascript",
+    },
+    {
+      id: 4,
+      name: "Defininte Guide with Javascript",
+    },
+    {
+      id: 5,
+      name: "Defininte Guide with Javascript",
+    },
+  ];
+
+  const icons = [
+    { id: 1, icons: <FaRegFaceLaughBeam /> },
+    { id: 2, icons: <FaRegFaceKissWinkHeart /> },
+    { id: 3, icons: <FaRegFaceGrinTongueSquint /> },
+    { id: 4, icons: <FaRegFaceSadCry /> },
+  ];
+
+  const handleShow = (id: number) => {
+    setShowIcon(true);
+    setSelectedIcon(id);
+    console.log(id);
+  };
+
+  const handleIcon = (id: number) => {
+    setIconList(id);
+    setSelectedIcon(id);
+    setShowIcon(false);
+  };
+  return (
+    <div className={classes.PageContainer}>
+      <div className={classes.Container}>
+        <h1 className={classes.heading}>My Interested Technical Book</h1>
+
+        <div className={classes.bookList}>
+          {booklist.map((item, index) => (
+            <div key={index}>
+              <div className={classes.book}>
+                <p>{item.name}</p>
+              </div>
+              <div className={classes.bookComment}>
+                <div className={classes.likeButton}>
+                  {" "}
+                  <p style={{ color: "yellow" }}>
+                    {iconList ? icons[selectedIcon].icons : ""}
+                  </p>
+                  <p onClick={() => handleShow(item.id)} key={index}>
+                    Like
+                  </p>
+                  <div>
+                    {" "}
+                    {showIcon && selectedIcon === item.id ? (
+                      <div className={classes.icons}>
+                        {icons.map((item, index) => (
+                          <div key={index} onClick={() => handleIcon(item.id)}>
+                            {item.icons}
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+
+                <p>Comment</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
